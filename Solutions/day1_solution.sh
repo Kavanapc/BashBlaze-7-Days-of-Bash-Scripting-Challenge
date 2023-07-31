63a968527dc460cfe17452aa0546618cb3354fe3
kavana <sharat@123.gmail>
2023-07-31 09:50:36 +0000

    solution

--- Solutions/day1_solution.sh
+++ Solutions/day1_solution.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-#Task-1:Welcome to Day 1 of the Bash Scripting Challenge! 
+
+#Task-1: basics of bash scripting 
+
 #Task-2:The echo command in shell scripting is used to display messages or output to the terminal or console. It allows you to print text or variables to the screen, which is particularly useful for providing information or feedback to users or for debugging purposes.
 echo "here is the solution for day 1 of Bash Scripting Challenge"
+
 #Task-3:variable assignment
 a=10
 b=20
+
 #Task-4:Accessing variables:Variable data could be accessed by appending the variable name with ‘$’ 
 echo "sum of given numbers is $((a+b))"
+
 #Task-5:built-in variables
 echo "Username of current user is: $USER "
 echo "Home directory of current user is: $HOME"
 echo "version of bash shell is: $BASH_VERSION"
 echo "Script name: $0"
+
 #Task-6:Wildcards:Wildcards are special characters used to perform pattern matching when working with files.Here * is used to list all the file with .txt extenion in current working directory
 ls *.txt
-
+#Bonus point: To create multiple files in single command use touch file{1..6}.txt
 
 
 
